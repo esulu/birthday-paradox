79af6bfc18d82afe0bd629bc45ee7b35f9b86e94
Eren Sulutas <erensulutas@hotmail.com>
2019-07-31 21:19:44 -0400

    final commit

--- birth.py
+++ birth.py
@@ -1,14 +1,18 @@
 import random
 
-subjects = 10
+subjects = 100
 birthday_list = []
+matching = False
 
 for i in range(subjects):
     birthday = random.randint(0, 365)
 
     if birthday in birthday_list:
-        print("Matching birthdays found with {} subjects".format(i))
+        print("\nMatching birthdays found at {} subjects\n".format(i))
+        matching = True
         break
     else:
         birthday_list.append(birthday)
 
+if not matching:
+    print("\nNo matching birthdays found with {} subjects\n".format(subjects))
