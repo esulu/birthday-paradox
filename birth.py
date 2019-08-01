d2047799189110ed8bc6e76e8ad07680fb03ccb4
Eren Sulutas <erensulutas@hotmail.com>
2019-07-31 21:15:51 -0400

    generator fix

--- birth.py
+++ birth.py
@@ -4,9 +4,11 @@ subjects = 10
 birthday_list = []
 
 for i in range(subjects):
-    birthday = random.randint(366)
+    birthday = random.randint(0, 365)
 
     if birthday in birthday_list:
         print("Matching birthdays found with {} subjects".format(i))
+        break
+    else:
+        birthday_list.append(birthday)
 
-    
